1f3d2964bddfb90b88afe9bcb6782870246b8992
Deep-Howlround <odonnell.abu@gmail.com>
2017-03-09 14:18:56 -0500

    add database

--- routes.py
+++ routes.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/cacaphon-learnflask'
+db.init_app(app)
 @app.route("/")
 def index():
     return render_template("index.html")
